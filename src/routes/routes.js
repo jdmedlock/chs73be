6acf700d7f09f88babbe5527d8b5cf6a02548113
Jim Medlock <jdmedlock@gmail.com>
2022-05-14 19:47:27 -0500

    Feature: Add controller to retrieve classmates from Airtable

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const { wakeUp } = require('../controllers/wakeup');
+const express = require('express')
+const { getClassmatesJSON } = require('../controllers/classmates')
+const { wakeUp } = require('../controllers/wakeup')
 
-const router = express.Router();
+const router = express.Router()
 
+router.route('/classmates')
+  .get(getClassmatesJSON)
 router.route('/wakeup')
-    .get(wakeUp)
+  .get(wakeUp)
 
 module.exports = router
