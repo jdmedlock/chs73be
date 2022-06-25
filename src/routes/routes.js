29ac2c1c93ac611ba02c78908270e6c831e5722d
jdmedlock <jdmedlock@gmail.com>
2022-06-25 13:11:57 -0500

    Feature: Add faculty route and controller

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const { getClassmates } = require('../controllers/classmates')
+const { getFaculty } = require('../controllers/faculty')
 const { wakeUp } = require('../controllers/wakeup')
 
 const router = express.Router()
 
 router.route('/classmates')
   .get(getClassmates)
+router.route('/faculty')
+  .get(getFaculty)
 router.route('/wakeup')
   .get(wakeUp)
 
