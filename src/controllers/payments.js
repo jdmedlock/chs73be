4cc6542c381cbc249ce6026adc1f828c532920c3
jdmedlock <jdmedlock@gmail.com>
2022-10-30 07:53:11 -0500

    Feature: Add basic logPayment route and controller

--- src/controllers/payments.js
+++ src/controllers/payments.js
@@ -4,7 +4,7 @@ const asyncHandler = require('express-async-handler')
 const logPayment = (req, res) => {
   const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE)
   const orderID = req.body.orderid
-  res.send("Payment logged")   
+  res.set("ok", true).status(200).send("Payment logged")   
 }
 
 exports.logPayment = logPayment 
