33889bad79d800b0371c82f98f1a41aa8c3e1cb5
Jim <jdmedlock@gmail.com>
2025-06-08 09:37:10 -0500

    Feature: Refactor sendEventAck.js to use new Mailjet template

--- src/controllers/sendEventAck.js
+++ src/controllers/sendEventAck.js
@@ -1,26 +1,17 @@
 const nodemailjet = require("node-mailjet")
 
 const sendEventAck = async (req, res) => {
-  const { order_id, item_description, event_date, 
-    order_amount, transaction_status, transaction_creation_time, 
-    payer_email_address, payer_firstname, payer_lastname, 
-    shipping_address_line_1, shipping_address_line_2, 
-    shipping_city, shipping_state, shipping_postal_code, is_sponsor, is_veteran,
+  const { order_id, item_description,
+    transaction_status, transaction_creation_time, payer_email_address, 
     classmateFirstName, classmateLastName, companionFirstName, companionLastName } = req.body
 
   const mailjet = nodemailjet
     .connect(process.env.MAILJET_API_KEY, process.env.MAILJET_SECRET_KEY)
 
-  console.log(`\nAcknowledging payment for ${ item_description } from ${ payer_firstname } ${ payer_lastname} (${ payer_email_address })`)
+  console.log(`\nAcknowledging payment for ${ item_description } from ${ classmateFirstName } ${ classmateLastName } (${ payer_email_address })`)
 
   if(item_description === '52nd Reunion Picnic') {
-    let paymentInstructionLine1 = ''
-    let paymentInstructionLine2 = ''
-    let paymentInstructionLine3 = ''
-    let paymentInstructionLine4 = ''
-    let paymentInstructionLine5 = ''
-    let paymentInstructionLine6 = ''
-
+    console.log(`\nSending payment acknowledgement email to ${ payer_email_address }`)
     const mailjetReq = mailjet
       .post("send", {'version': 'v3.1'})
       .request({
@@ -33,33 +24,24 @@ const sendEventAck = async (req, res) => {
             "To": [
               {
                 "Email": `${ payer_email_address }`,
-                "Name": `${ payer_firstname } ${ payer_lastname }`
+                "Name": `${ classmateFirstName } ${ classmateLastName }`
               }
             ],
-            "TemplateID": 4560572,
+            "TemplateID": 7054789,
             "TemplateLanguage": true,
             "Subject": "CHS73 52nd Reunion - You are registered for the Saturday gathering",
             "Variables": {
-              "classmateFirstName": `${ payer_firstname }`,
-              "classmateLastName": `${ payer_lastname }`,
+              "classmateFirstName": `${ classmateFirstName }`,
+              "classmateLastName": `${ classmateLastName }`,
               "registrationId": `${ order_id }`,
-              "orderAmount": `${ order_amount }`,
               "transactionStatus": `${ transaction_status }`,
               "transactionCreated": `${ transaction_creation_time }`,
               "cleaning": "True",
               "classmateEmail": `${ payer_email_address }`,
-              "isSponsor": `${ is_sponsor }`,
-              "isVeteran": `${ is_veteran }`,
               "classmateBadgeFirstName": `${ classmateFirstName }`,
               "classmateBadgeLastName": `${ classmateLastName }`,
               "companionBadgeFirstName": `${ companionFirstName }`,
               "companionBadgeLastName": `${ companionLastName }`,
-              "paymentInstructionLine1": paymentInstructionLine1,
-              "paymentInstructionLine2": paymentInstructionLine2,
-              "paymentInstructionLine3": paymentInstructionLine3,
-              "paymentInstructionLine4": paymentInstructionLine4,
-              "paymentInstructionLine5": paymentInstructionLine5,
-              "paymentInstructionLine6": paymentInstructionLine6,
             }
           }
         ]
